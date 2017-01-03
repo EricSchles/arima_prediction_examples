b88ebc1e145678b48c70a52991f20dfa95f54c0d
Eric Schles <ericschles@gmail.com>
2017-01-03 16:12:40 -0500

    pushing just in case

--- ridership.py
+++ ridership.py
@@ -20,18 +20,8 @@ df.index.name=None
 
 df.columns = ['riders']
 df['riders'] = df.riders.apply(lambda x: int(x) * 100)
-
-# decomposition = seasonal_decompose(df.riders, freq=12)
-
-# df.riders_log = df.riders.apply(lambda x: np.log(x))
-# df['first_difference'] = df.riders - df.riders.shift(1)
-
-# df['log_first_difference'] = df.riders_log - df.riders_log.shift(1)
-# df["seasonal_difference"] = df.riders - df.riders.shift(12)
-# df["log_seasonal_difference"] = df.riders_log - df.riders_log.shift(12)
-# df["seasonal_first_difference"] = df.first_difference - df.first_difference.shift(12)
-# df["log_seasonal_first_difference"] = df.log_first_difference - df.log_first_difference.shift(12)
-
+import code
+code.interact(local=locals())
 model = sm.tsa.statespace.SARIMAX(df.riders, trend='n', order=(0, 1, 0), seasonal_order=(0, 1, 1, 12))
 results = model.fit()
 df['forecast'] = results.predict(start = 102, end = 114, dynamic = True)
